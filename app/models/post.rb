b79f951a8f82015131b43afd02a223b9ad0b0c7f
Morrism1 <morrismontana0@gmail.com>
2020-09-03 17:12:38 +0200

    Create association between post, user and comment models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 2, maximum: 30 }, uniqueness: true
   validates :body, presence: true
 end
